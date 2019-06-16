a62acda2428f6a2dfc8f8cb7ff0d933511e092ae
fabio henrique da silva <hantarodev@gmailcom>
2019-06-16 18:39:12 -0300

    NewsPagerAdapter added

---
+++ app/src/main/java/hantaro/com/news/NewsPagerAdapter.java
@@ -0,0 +1,13 @@
+package hantaro.com.news;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
+
+public class NewsPagerAdapter extends ViewPager {
+    public NewsPagerAdapter(@NonNull Context context) {
+        super(context);
+    }
+
+
+}
